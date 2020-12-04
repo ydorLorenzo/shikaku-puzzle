3960f9efbdb5411e43dca4916343587d9000fee8
rodylc <rodylorenzo93@gmail.com>
2020-12-04 15:53:46 +0100

    Using regex to get the values of the clues for the input

--- shikaku_converter/main.py
+++ shikaku_converter/main.py
@@ -1,44 +1,50 @@
 from os import walk
 
-import itertools
 import math
-import string
+import re
+
+
+def string2int(s):
+    num = 0
+    for c in s:
+        x = ord(c) - ord('`')
+        num += x if x > 0 else 0
+    return num
 
 
 def task_to_array(task_file):
     file = open(task_file, "r")
     file_content = file.read()
-    array = []
-    for char in file_content:
-        if char.isdigit():
-            array.append(char)
-        elif char.isalpha():
-            index = string.ascii_lowercase.index(char)
-            index += 1
-            for _ in itertools.repeat(None, index):
-                array.append(" ")
-    return array
+    _numbers = re.findall(r'\d+', file_content)
+    literals = re.findall(r'\D+', file_content)
+    if file_content[0].isdigit():
+        literals = ['_'] + literals
+    _spaces = []
+    for elm in literals:
+        _spaces.append(string2int(elm))
+    return _numbers, _spaces, int(math.sqrt(sum(_spaces) + len(_numbers)))
 
 
-def array_to_datafile(array, origin):
-    side_length = int(math.sqrt(len(array)))
+# todo check for the monthly, daily and weekly puzzles because it's not a square problem
+def array_to_datafile(numbers, spaces, n, origin):
     filename = str(origin).split(".")[0] + ".dzn"
     file = open(f"resources/models/{filename}", "w")
-    file.write(f"size = {side_length};\n\n")
+    file.write(f"size = {n};\n\n")
     file.write("given = [|")
-    index = 0
-    for element in array:
-        if element.isdigit():
-            row = (index // side_length) + 1
-            col = (index % side_length) + 1
-            file.write(f"\n{row},{col},{element}|")
-        index += 1
-    file.write("];")
+    cumulative_index = 0
+    for index, numb in enumerate(numbers):
+        cumulative_index += spaces[index] + 1
+        row = math.ceil(cumulative_index/n)
+        col = cumulative_index % n
+        if col == 0:
+            col = n
+        file.write(f"\n{row}, {col}, {numb}|")
+    file.write("];\n")
+    file.close()
 
 
 if __name__ == '__main__':
-    f = []
     for (dirpath, dirnames, filenames) in walk("resources/tasks"):
         for filename in filenames:
-            array = task_to_array(f"{dirpath}/{filename}")
-            array_to_datafile(array, filename)
+            numbs, sps, n = task_to_array(f"{dirpath}/{filename}")
+            array_to_datafile(numbs, sps, n, filename)
